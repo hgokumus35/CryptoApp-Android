b26fa7fe9f4118d71d95eec4820f4a12c44a9687
hgokumus35 <h.g.okumus9898@hotmail.com>
2024-04-04 22:03:32 +0300

    Project architecture created

---
+++ app/src/main/java/com/hgokumus/cryptoapp/CryptoApp.kt
@@ -0,0 +1,19 @@
+package com.hgokumus.cryptoapp
+
+import android.app.Application
+import com.hgokumus.cryptoapp.home.di.HomeDI
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class CryptoApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger(Level.ERROR)
+            androidContext(this@CryptoApp)
+            modules(listOf(HomeDI.module))
+        }
+    }
+}
---
+++ app/src/main/java/com/hgokumus/cryptoapp/home/HomeActivity.kt
@@ -0,0 +1,19 @@
+package com.hgokumus.cryptoapp.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hgokumus.cryptoapp.R
+import com.hgokumus.cryptoapp.cryptoList.CryptoListFragment
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.homeContainer, CryptoListFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
